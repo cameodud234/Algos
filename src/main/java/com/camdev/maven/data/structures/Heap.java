aead4c60350902a9a0aec1e509116a1d88fe0786
sunniestAcorn00 <30845397+cameodud234@users.noreply.github.com>
2022-10-16 12:53:48 -0700

    Update

--- src/main/java/com/camdev/maven/data/structures/Heap.java
+++ src/main/java/com/camdev/maven/data/structures/Heap.java
@@ -1,5 +1,18 @@
 package com.camdev.maven.data.structures;
 
-public class Heap {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class Heap<T extends Comparable<T>> {
+
+    private List<T> elements;
+    
+    private static final int DEFAULT_CAPACITY = 10;
+    private final List<T> EMPTY_ELEMENTSDATA = List.of();
+    
+    public Heap() {
+        elements = Arrays.asList(EMPTY_ELEMENTSDATA);
+    }
+    
 }
